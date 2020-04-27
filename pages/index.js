9cc06af91a134748cc08275acdcb123975850b54
panhara <titpanhara@gmail.com>
2020-04-27 14:37:44 +0700

    new file:   .gitignore 	new file:   package-lock.json new file:   package.json 	new file:   pages/index.js 	new file:   pages/layouts/Header.js 	new file:   public/favicon.ico 	new file:   public/zeit.svg 	new file:   yarn.lock

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+
+import Header from './layouts/Header';
+
+const Index = () => (
+  <>
+    <Header />
+
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>Welcome to News App!</h1>
+      <p>Indonesia News in Indonesia Section, Singapore in Singapore Section and United States in US Section</p>
+    </main>
+  </>
+)
+
+export default Index
